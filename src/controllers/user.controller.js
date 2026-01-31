1cce8e800cb1590c2490531b913001d41c3a3fa1
Jitender <jitenderyadav330@gmail.com>
2026-01-31 13:03:14 +0530

    user create, get, delete done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -9,6 +9,20 @@ export const getUsers = async (req, res) => {
   }
 };
 
+
+// get single user
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+
+// Create User 
 export const createUser = async (req, res) => {
   try {
     const { userId, name, email } = req.body;
@@ -31,5 +45,32 @@ export const createUser = async (req, res) => {
     res.status(201).json(user);
   } catch (error) {
     res.status(500).json({ message: "User creation failed" });
+  }
+};
+
+
+
+// Update single user
+export const updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+
+// Delete user
+export const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.json({ message: "User deleted successfully" }); 
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { getUsers, createUser } from "../controllers/user.controller.js";
+import {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser 
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.get("/", getUsers);
-router.post("/", createUser);
+router.get("/", getUsers);              // get all users
+router.get("/:id", getUserById);        // get single user
+router.post("/", createUser);           // add user
+router.put("/:id", updateUser);         // update user
+router.delete("/:id", deleteUser);
 
 export default router;
